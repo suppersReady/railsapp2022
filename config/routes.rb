3cba7e5502b820a4825f214457e84bbed639ae5c
railsapp2022 <s8ni1@yahoo.it>
2022-11-18 09:58:38 +0100

    Creato contenuti

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  resources :contents
   devise_for :utentis
-  devise_for :users
+  devise_for :user
   root 'pages#home'
 
   get 'about' => 'pages#about'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_230227) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_224822) do
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "utentis", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
